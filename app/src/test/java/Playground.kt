80b6aca1420087cba7e37d51b4d3dd15ebbf2ee9
MyIDDQD <nickkosh@gmail.com>
2023-01-09 13:23:42 +0100

    done kata

--- app/src/test/java/Playground.kt
+++ app/src/test/java/Playground.kt
@@ -1,4 +1,15 @@
 fun main() {
-
+    println(fizzBuzz(10).joinToString(" ")) // "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"
+    println(fizzBuzz(1).joinToString( " ")) // "1"
 }
 
+fun fizzBuzz(n: Int): Array<String> {
+    return IntArray(n) { it + 1 }.toMutableList().map {
+        when {
+            it % 3 == 0 && it % 5 == 0 -> "FizzBuzz"
+            it % 3 == 0 -> "Fizz"
+            it % 5 == 0 -> "Buzz"
+            else -> it.toString()
+        }
+    }.toTypedArray()
+}
