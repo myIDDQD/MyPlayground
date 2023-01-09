0a87a1e60656d828cafd6d9bfbc77f347a53c302
MyIDDQD <nickkosh@gmail.com>
2023-01-09 13:10:39 +0100

    done kata

--- app/src/test/java/Playground.kt
+++ app/src/test/java/Playground.kt
@@ -1,3 +1,11 @@
 fun main() {
-
+    angle(3)
+    angle(4)
 }
+
+fun angle(n: Int) = (n * 180) - 360
+
+//fun angle(n: Int): Int {
+//    println((n * 180) - 360)
+//    return (n * 180) - 360
+//}
