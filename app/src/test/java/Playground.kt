9c64bb8f29d90a07f5b847580944075dc1d29a97
MyIDDQD <nickkosh@gmail.com>
2023-01-10 16:49:57 +0100

    start Speed Control

--- app/src/test/java/Playground.kt
+++ app/src/test/java/Playground.kt
@@ -1,4 +1,10 @@
 fun main() {
-
+    var x = doubleArrayOf(0.0, 0.23, 0.46, 0.69, 0.92, 1.15, 1.38, 1.61)
+    gps(20, x) // 41
+    x = doubleArrayOf(0.0, 0.11, 0.22, 0.33, 0.44, 0.65, 1.08, 1.26, 1.68, 1.89, 2.1, 2.31, 2.52, 3.25)
+    gps(12, x) // 219
 }
 
+fun gps(s:Int, x:DoubleArray):Int {
+    // your code
+}
