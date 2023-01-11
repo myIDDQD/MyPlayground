04330fd5d7cd90bc419232790c9987993edc3531
MyIDDQD <nickkosh@gmail.com>
2023-01-11 11:10:24 +0100

    done Going to the cinema

--- app/src/test/java/Playground.kt
+++ app/src/test/java/Playground.kt
@@ -1,3 +1,5 @@
+import kotlin.math.ceil
+
 // https://www.codewars.com/kata/562f91ff6a8b77dfe900006e/train/kotlin
 
 // My friend John likes to go to the cinema. He can choose between system A and system B.
@@ -11,15 +13,47 @@
 // System B : 500 + 15 * 0.90 + (15 * 0.90) * 0.90 + (15 * 0.90 * 0.90) * 0.90 ( = 536.5849999999999, no rounding for each ticket)
 // John wants to know how many times he must go to the cinema so that the final result of System B, when rounded up to the next dollar, will be cheaper than System A.
 //
-// The function movie has 3 parameters: card (price of the card), ticket (normal price of a ticket), perc (fraction of what he paid for the previous ticket) and returns the first n such that
+// The function movie has 3 parameters:
+// card (price of the card),
+// ticket (normal price of a ticket),
+// perc (fraction of what he paid for the previous ticket)
+// and returns the first n such that
 
+// More examples:
+// movie(500, 15, 0.9) should return 43 (with card the total price is 634, with tickets 645)
+// movie(100, 10, 0.95) should return 24 (with card the total price is 235, with tickets 240)
 
 fun main() {
-    //tests
+    // Fixed Tests
     movie(500, 15, 0.9) // 43
     movie(100, 10, 0.95) // 24
+    movie(0, 10, 0.95) // 2
+
+    // Random Tests
+    movie(478136, 11, 0.86) // 43474
+    movie(578090, 29, 0.46) // 19936
 }
 
 fun movie(card: Int, ticket: Int, perc: Double) : Int {
-    return 0
+    var ticketsTotal = 0
+    var cardDiscount = card.toDouble()
+    var ticketPriceDiscount = ticket * perc
+    var count = 0
+
+    while (ticketsTotal <= ceil(cardDiscount)) {
+        ticketsTotal += ticket
+        cardDiscount += ticketPriceDiscount
+        ticketPriceDiscount *= perc
+        count += 1
+
+// debugging
+//        println("ticketsTotal = $ticketsTotal --- " +
+//                "cardDiscount = $cardDiscount --- " +
+//                "ticketPriceDiscount = $ticketPriceDiscount --- " +
+//                "count = $count")
+    }
+
+// debugging
+    println(count)
+    return count
 }
