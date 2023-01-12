866029b82c09192fe1b53ac655bba2c13309e0c4
MyIDDQD <nickkosh@gmail.com>
2023-01-12 10:58:11 +0100

    add InviteMoreWomen

--- app/src/test/java/Playground.kt
+++ app/src/test/java/Playground.kt
@@ -1,11 +1,4 @@
 fun main() {
-    angle(3)
-    angle(4)
+
 }
 
-fun angle(n: Int) = (n * 180) - 360
-
-//fun angle(n: Int): Int {
-//    println((n * 180) - 360)
-//    return (n * 180) - 360
-//}
