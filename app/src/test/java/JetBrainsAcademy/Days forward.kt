ea21bb7af3df227985b522a315c1208df0d10e01
MyIDDQD <nickkosh@gmail.com>
2023-01-18 11:53:02 +0100

    end JetBrainsAcademy Local date and time Days forward
    Use java.time.LocalDate
    because can't use kotlinx.datetime.*

--- app/src/test/java/JetBrainsAcademy/Days forward.kt
+++ app/src/test/java/JetBrainsAcademy/Days forward.kt
@@ -1,2 +1,15 @@
 package JetBrainsAcademy
 
+import java.time.LocalDate
+
+fun daysForward(date: String, days: Int) {
+    val localDate = LocalDate.parse(date)
+    val daysForward = localDate.plusDays(days.toLong())
+    println(daysForward.toString())
+}
+
+fun main() {
+    val date = readLine()!!
+    val days = readLine()!!.toInt()
+    daysForward(date, days)
+}
