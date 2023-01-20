be5142ca4a9ee755304cf273a5a07e8c85ccb7cc
MyIDDQD <nickkosh@gmail.com>
2023-01-20 09:28:39 +0100

    add Codewars Predict your age!

---
+++ app/src/test/java/Codewars/Predict your age.kt
@@ -0,0 +1,25 @@
+package Codewars
+// Predict your age!
+// https://www.codewars.com/kata/5aff237c578a14752d0035ae/train/kotlin
+
+/*
+My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+
+In honor of my grandfather's memory we will write a function using his formula!
+
+Take a list of ages when each of your great-grandparent died.
+Multiply each number by itself.
+Add them all together.
+Take the square root of the result.
+Divide by two.
+Example
+predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+ */
+
+fun predictAge(age1: Int, age2: Int, age3: Int, age4: Int, age5: Int, age6: Int, age7: Int, age8: Int): Int{
+    return 0
+}
+
+fun main() {
+
+}
