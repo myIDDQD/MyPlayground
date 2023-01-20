73bfe0dc549ce32bcb4d91bff2e7514077ae1948
MyIDDQD <nickkosh@gmail.com>
2023-01-20 08:23:16 +0100

    start Codewars Alphabetical Addition

--- app/src/test/java/Codewars/Alphabetical Addition.kt
+++ app/src/test/java/Codewars/Alphabetical Addition.kt
@@ -2,3 +2,41 @@ package Codewars
 
 // Alphabetical Addition
 // https://www.codewars.com/kata/5d50e3914861a500121e1958/train/kotlin
+
+/*
+Your task is to add up letters to one letter.
+
+The function will be given a List<Char>, each one being a letter to add, and the function will return a Char.
+
+Notes:
+Letters will always be lowercase.
+Letters can overflow (see second to last example of the description)
+If no letters are given, the function should return 'z'
+
+
+My notes
+a = 1
+b = 2
+c = 3
+d = 4
+.
+.
+.
+y = 25
+z = 26
+ */
+
+fun addLetters(arr: List<Char>): Char {
+    // your code here
+    return 'a'
+}
+
+fun main() {
+    addLetters(listOf('a', 'b', 'c')) // 'f'
+    addLetters(listOf('z')) // 'z'
+    addLetters(listOf('a', 'b')) // 'c'
+    addLetters(listOf('c')) // 'c'
+    addLetters(listOf('z', 'a')) // 'a'
+    addLetters(listOf('y', 'c', 'b')) // 'd'
+    addLetters(listOf()) // 'z'
+}
