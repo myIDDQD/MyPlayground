42a1d3747e371008592f1c616cb359179d9fb8f5
MyIDDQD <nickkosh@gmail.com>
2023-02-06 10:40:55 +0100

    adds Codewars Multiplication table

---
+++ app/src/test/java/Codewars/Multiplication table.kt
@@ -0,0 +1,18 @@
+package Codewars
+
+// Multiplication table
+// https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/train/kotlin
+
+/* DESCRIPTION
+Your task, is to create N×N multiplication table, of size provided in parameter.
+
+For example, when given size is 3:
+
+1 2 3
+2 4 6
+3 6 9
+For the given example, the return value should be:
+
+[[1,2,3],[2,4,6],[3,6,9]]
+ */
+
