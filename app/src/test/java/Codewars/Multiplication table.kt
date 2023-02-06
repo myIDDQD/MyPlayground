e4b83b26a8dcf063cb87319d8a454939c627f5fe
MyIDDQD <nickkosh@gmail.com>
2023-02-06 10:50:56 +0100

    done Codewars Multiplication table

--- app/src/test/java/Codewars/Multiplication table.kt
+++ app/src/test/java/Codewars/Multiplication table.kt
@@ -16,3 +16,19 @@ For the given example, the return value should be:
 [[1,2,3],[2,4,6],[3,6,9]]
  */
 
+/* ChatGPT
+Solved it using ChatGPT, it turned out quite quickly :)
+At a minimum, ChatGPT writes the basis of the solution, which needs to be slightly corrected
+ */
+
+object KataSolution {
+    fun multiplicationTable(size: Int): Array<IntArray> {
+        val table = Array(size) { IntArray(size) }
+        for (i in 0 until size) {
+            for (j in 0 until size) {
+                table[i][j] = (i + 1) * (j + 1)
+            }
+        }
+        return table
+    }
+}
