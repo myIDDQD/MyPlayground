81c2b7cdca883392ab5b47bcd08a31e0910486f4
Mykola <nickkosh@gmail.com>
2023-02-20 09:43:06 +0200

    done Mexican Wave Codewars

--- app/src/main/java/Codewars/Mexican Wave.kt
+++ app/src/main/java/Codewars/Mexican Wave.kt
@@ -12,3 +12,29 @@ Rules
 Example
 wave("hello") => {"Hello", "hEllo", "heLlo", "helLo", "hellO"}
  */
+
+fun main() {
+    wave("a       b    ").let(::println) // listOf("A       b    ", "a       B    ")
+    wave("this is a few words")
+/*
+listOf("This is a few words", "tHis is a few words", "thIs is a few words", "thiS is a few words", "this Is a few words", "this iS a few words", "this is A few words", "this is a Few words", "this is a fEw words", "this is a feW words", "this is a few Words", "this is a few wOrds", "this is a few woRds", "this is a few worDs", "this is a few wordS")
+ */
+    wave("").let(::println) // listOf<String>()
+    wave(" gap ").let(::println) // listOf(" Gap ", " gAp ", " gaP ")
+}
+
+fun wave(str: String): List<String> {
+    val resultList = mutableListOf<String>()
+
+    for (i in str.indices) {
+        var tempStr = ""
+        for ((index, c) in str.lowercase().withIndex()) {
+            tempStr += if (index == i) c.uppercase() else c
+        }
+        resultList.add(tempStr)
+    }
+
+    resultList.removeAll(listOf(str))
+
+    return resultList
+}
