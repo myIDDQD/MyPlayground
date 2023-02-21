8e948cd138a13c7ed0d5f73e9e0b3873b28f6d4e
Mykola <nickkosh@gmail.com>
2023-02-21 14:52:14 +0200

    Begin Which are in? Codewars

--- app/src/test/java/Playground.kt
+++ app/src/test/java/Playground.kt
@@ -1,13 +1,11 @@
-fun helpingTheRobot(purchases: Map<String, Int>, addition: Map<String, Int>) : MutableMap<String, Int> {
-    val purchase = (purchases + addition).toMutableMap()
-    purchase.map { if (purchases.keys.contains(it.key) && addition.keys.contains(it.key)) purchase[it.key] = purchases[it.key]!! + addition[it.key]!! }
-    return purchase
-}
-
 fun main() {
-    val purchases = mutableMapOf("bread" to 1, "potatoes" to 7, "milk" to 2)
-    val addition = mutableMapOf("milk" to 1, "water" to 3)
-    println(helpingTheRobot(purchases, addition))
-    println("{bread=1, potatoes=7, milk=3, water=3}")
+    // Read input from user
+    val exabytes = readLine()!!.toDouble()
+
+    // Convert exabytes to bits
+    val bits = exabytes.pow(63)
+
+    // Print result
+    println(bits)
 }
 
